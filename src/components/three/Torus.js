7fc0a86423d760e2f91ec609b97d209ddcab2fc9
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-02-08 22:58:49 +0100

    update torus geometry

--- src/components/three/Torus.js
+++ src/components/three/Torus.js
@@ -5,9 +5,10 @@ import { MeshStandardMaterial, TextureLoader } from 'three'
 import * as THREE from 'three'
 import { useTexture } from '@react-three/drei'
 
+
 function Torus(props){
     const ref = useRef()
-    //useFrame(() => (ref.current.rotation.x += 0.01, ref.current.roation.y += 0.01))
+    useFrame((state, delta) => (ref.current.rotation.x += delta))
 
     const displacementMap = useTexture('//cdn.wtlstudio.com/sample.wtlstudio.com/827bc230-6686-4cc3-9389-4382d81e927a.png', (texture)=>
     {
@@ -22,7 +23,7 @@ function Torus(props){
         {...props}
         ref = {ref}
         rotation={[0, 1.57079633, 0]}>
-        <torusGeometry args = {[3, 0.0000000001, 16, 100]}/>
+        <torusGeometry args = {[10, 0.0000000001, 100, 100]}/>
         <meshStandardMaterial displacementMap={displacementMap} displacementScale={.5} />
         </mesh>
     )
