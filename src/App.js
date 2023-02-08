e1a57209a5403fd01c1d7858f44b73ac613a353f
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-02-08 23:00:58 +0100

    add camera helper

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import Torus from './components/three/Torus';
 import { OrbitControls } from '@react-three/drei'
+//import { PerspectiveCamera } from '@react-three/drei'
+import { CameraHelper } from 'three';
+import { PerspectiveCamera } from 'three';
 
 function App() {
+  const config = {fov: 55, position: [11,100,40.]}
+
+  function CameraHelper(){
+    const camera = new PerspectiveCamera(60,1,1,9)
+    return <cameraHelper args = {[camera]}/>
+  }
+
   return (
-    <Canvas>
+    <Canvas camera = {{position: [0,-10,-10], fov: 90}}>
       <OrbitControls rotateSpeed={0} autoRotate={false}/>
       <ambientLight></ambientLight>
       <pointLight position={[10, 10, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
       <Torus position = {[0, 0, 0 ]}></Torus>
+      <CameraHelper/>
     </Canvas>
   );
 }
