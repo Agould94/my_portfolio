f08d27d2acc26a264cc4d1835bafb297149edb1c
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-02-08 23:00:21 +0100

    Update index to display alt app and use browserrouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-import {useRef, useState} from 'react'
-
-import {Canvas, useFrame} from '@react-three/fiber'
+import AppAlt from './AppAlt';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+    <BrowserRouter>
+      <AppAlt />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
