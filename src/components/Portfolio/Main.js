39381ad6cfc3c5f13a20e99ba4592d4e514d9109
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-02-14 01:48:27 +0100

    add skills, projects, state management to main.

--- src/components/Portfolio/Main.js
+++ src/components/Portfolio/Main.js
@@ -1,24 +1,41 @@
-import React,{useState, useEffect} from 'react'
+import React from 'react'
+import { useEffect, useState } from 'react'
 import NavBar from './NavBar'
 import Info from './Info'
 import BlogList from './BlogList'
+import Projects from './Projects'
+import Skills from './Skills'
+
+
 
 function Main() {
 
   const [info, setInfo] = useState({})
+  const [loading, setLoading]=useState(true)
   console.log(info)
 
   useEffect(()=>{
+    console.log("effect")
     fetch("http://localhost:4000/info")
     .then((r)=>r.json())
-    .then((info)=>setInfo(info[0]))
+    .then((info)=>{
+      setInfo(info[0]) 
+      setLoading(false)})
   },[])
 
   return (
     <div>
-      <NavBar></NavBar>
+      {loading 
+      ?
+      null
+      :
+      <div>
       <Info info = {info}></Info>
+      <Skills skills = {info.skills} technologies={info.technologies}></Skills>
+      <Projects></Projects>
       <BlogList></BlogList>
+      </div>
+      }
     </div>
   )
 }
